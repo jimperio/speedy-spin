6c110451498ab913be840de2dbbcc17e8e5a6edb
Jomel Imperio <jimperio@gmail.com>
2014-07-24 12:45:10 +0800

    Added simple ledge spawning and increased starting speed.

--- main.js
+++ main.js
@@ -1,59 +1,75 @@
 function init(parent) {
-	var state = {
-		preload: preload,
-		create: create,
-		update: update
-	};
+  var state = {
+    preload: preload,
+    create: create,
+    update: update
+  };
 
-	var game = new Phaser.Game(
-		300,
-		400,
-		Phaser.AUTO,
-		parent,
-		state,
-		false,
-		false
-	);
+  var game = new Phaser.Game(
+    300,
+    400,
+    Phaser.AUTO,
+    parent,
+    state,
+    false,
+    false
+  );
 
-	var player,
-	    cursors,
-	    platforms,
-	    currFallSpeed = -50;
+  var player,
+      cursors,
+      platforms,
+      currFallSpeed = -100,
+      ledgeTimer,
+      hasSkippedLedge = false;
 
-	function preload() {
+  function preload() {
     game.load.image('star', 'assets/star.png');
-    game.load.image('ground', 'assets/platform.png');
-	}
+    game.load.image('ground', 'assets/ledge.png');
+    game.load.image('wall', 'assets/wall.png');
+  }
 
-	function create() {
+  function create() {
+    game.add.text(15, 
+                  0, 
+                  "3",
+                  {
+                    fill: "#fff",
+                  });
     player = game.add.sprite(32, 0, 'star');
 
     game.physics.arcade.enable(player);
 
-    player.body.bounce.y = 0.2;
     player.body.gravity.y = 300;
-    player.body.collideWorldBounds = true;
 
     cursors = game.input.keyboard.createCursorKeys();
 
-    // The ledges
+    //  Walls.
     platforms = game.add.group();
     platforms.enableBody = true;
-    var ledge = platforms.create(100, 150, 'ground');
-    ledge.scale.setTo(0.5,0.5);
-    ledge.body.velocity.y = currFallSpeed;
-    ledge.body.immovable = true;
 
-    ledge = platforms.create(-100, 200, 'ground');
-    ledge.scale.setTo(0.5,0.5);
-    ledge.body.velocity.y = currFallSpeed;
-    ledge.body.immovable = true;
-	}
+    var wall = platforms.create(0, 0, 'wall');
+    wall.body.immovable = true;
 
-	function update() {
-		game.physics.arcade.collide(player, platforms);
+    wall = platforms.create(290, 0, 'wall');
+    wall.body.immovable = true;
 
-		//  Reset the players velocity (movement)
+    // Setup ledge creation.
+    // TODO: Is there a more direct way to tie ledge generation
+    // to current speed? Ledges will speed up as game progresses.
+    ledgeGenerator = game.time.events.loop(550, spawnLedge);
+
+    var top = 150;
+    for (i = 0; i <= 5; i++) {
+      createLedge(getRandomLeft(), top);
+      top += 50;
+    }
+
+   }
+
+  function update() {
+    game.physics.arcade.collide(player, platforms);
+
+    //  Reset the players velocity (movement)
     player.body.velocity.x = 0;
 
     if (cursors.left.isDown)
@@ -66,7 +82,25 @@ function init(parent) {
         //  Move to the right
         player.body.velocity.x = 150;
     }
+  }
 
-	}
+  function spawnLedge(left, top) {
+    if (!hasSkippedLedge && Math.random() <= 0.25) {
+      hasSkippedLedge = true;
+      return;
+    }
+    hasSkippedLedge = false;
+    createLedge(getRandomLeft(), 400);
+  }
+
+  function getRandomLeft() {
+    return Math.random() * (300 - 90 - 20) + 10;
+  }
+
+  function createLedge(left, top) {
+    var ledge = platforms.create(left, top, 'ground');
+    ledge.body.velocity.y = currFallSpeed;
+    ledge.body.immovable = true;
+  }
 
 }
